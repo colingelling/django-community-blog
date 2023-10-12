3679d49cb47a0219dbca384e3c533853e1580a72
Colin Gelling <22787309+colingelling@users.noreply.github.com>
2023-10-12 09:59:06 +0200

    Added background functionality for form support into the database. The course has a practice exercise which shows what someone can do with the Python shell for a specific project. The functionality that was added (views.py) retrieves the data from the database, adds jobs to the form buttons and recognizes the switching between checkboxes.

--- test_project/main/views.py
+++ test_project/main/views.py
@@ -8,6 +8,27 @@ from .forms import CreateNewList
 
 def index(response, id):
     todo_list = TodoList.objects.get(id=id)
+
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+
+            for item in todo_list.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                todo_list.item_set.create(text=txt, complete=False)
+            else:
+                print("Invalid input")
+
     return render(response, "main/list.html", {"todo_list": todo_list})
 
 
@@ -19,12 +40,14 @@ def create(response):
     if response.method == "POST":
         form = CreateNewList(response.POST)
 
+        t = None
+
         if form.is_valid():
             n = form.cleaned_data["name"]
             t = TodoList(name=n)
             t.save()
 
-        return HttpResponseRedirect("/%i" %t.id)
+        return HttpResponseRedirect("/%i" % t.id)
 
     else:
         form = CreateNewList()
